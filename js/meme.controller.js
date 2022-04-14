06371aa3e806936cb33f8cc1729fb5e39c20c3bd
AdiZur <adizur9@gmail.com>
2022-04-14 20:00:03 +0300

    changes thursday

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,7 +1,6 @@
 'use ctrict'
-let gElCanvas, gCtx;
-
-renderMeme()
+let gElCanvas
+let gCtx
 
 function renderMeme() {
     gElCanvas = document.querySelector('#my-canvas')
@@ -22,19 +21,148 @@ function drawImg(meme, width, height) {
 }
 
 function drawText(meme, x, y) {
-    gCtx.font = `${meme.lines[0].size}px Impact`
-    gCtx.fillStyle = 'white'
-    gCtx.strokeStyle = 'black'
-    gCtx.textBaseline = 'Top'
-    gCtx.textAlign = `${meme.lines[0].align}`
-    gCtx.lineWidth = 2;
-    gCtx.fillText(meme.lines[0].txt, x, y);
-    gCtx.strokeText(meme.lines[0].txt, x, y);
-    gCtx.fill();
+    // console.log(x)
+    // console.log(y)
+    let lines = meme.lines
+    const idx = meme.selectedImgId
+    // console.log(idx)
+    const line = meme.lines[idx]
+    // console.log(txt)
+    lines.forEach(line => {
+        gCtx.font = `${line.size}px ${line.font}`
+        gCtx.fillStyle = line.color
+        gCtx.strokeStyle = 'black'
+        // gCtx.textBaseline = 'Top'
+        gCtx.textAlign = `${line.align}`
+        gCtx.lineWidth = 2;
+        gCtx.fillText(line.txt, line.x, line.y);
+        gCtx.strokeText(line.txt, line.x, line.y);
+        // gCtx.fill();
+    })
+    checkAlignFocus()
+
 }
 
 function onChangeLineTxt(value) {
-    setLineTxt(value)
+    if (document.querySelector('.txt-line').value) setLineTxt(value)
     renderMeme()
 }
 
+function onChangeTxtColor(color) {
+    console.log(color)
+    changeTxtColor(color)
+    renderMeme()
+}
+
+function onChangeFontSize(value) {
+    console.log('fontsize')
+    setFontSize(value)
+    renderMeme()
+}
+
+function onAddNewLine() {
+    console.log('newline')
+    meme = getMeme()
+    console.log(meme.selectedLineIdx)
+    addNewTextLine()
+    onSetFocus()
+    // switchLine()
+    // onSwitchLine()
+    renderMeme()
+}
+
+function onSwitchLine() {
+    let meme = getMeme()
+    onSetFocus()
+    console.log(meme.selectedLineIdx)
+    switchLine(meme.selectedLineIdx)
+    // console.log(newCurrLine)
+    // document.querySelector('.txt-line').value = meme.lines[newCurrLine].txt;
+    renderMeme()
+}
+
+function downloadCanvas(elLink) {
+    const data = gElCanvas.toDataURL()
+    elLink.href = data
+    elLink.download = 'canvas.jpg'
+}
+
+function uploadImg() {
+    const imgDataUrl = gElCanvas.toDataURL("image/jpeg");
+
+    // A function to be called if request succeeds
+    function onSuccess(uploadedImgUrl) {
+        const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+        document.querySelector('.user-msg').innerText = `Your photo is available here: ${uploadedImgUrl}`
+
+        document.querySelector('.share-container').innerHTML = `
+        <a class="btn" href="https://www.facebook.com/sharer/sharer.php?u=${encodedUploadedImgUrl}&t=${encodedUploadedImgUrl}" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}'); return false;">
+           Share   
+        </a>`
+    }
+    doUploadImg(imgDataUrl, onSuccess);
+}
+
+function doUploadImg(imgDataUrl, onSuccess) {
+
+    const formData = new FormData();
+    formData.append('img', imgDataUrl)
+
+    fetch('//ca-upload.com/here/upload.php', {
+        method: 'POST',
+        body: formData
+    })
+        .then(res => res.text())
+        .then((url) => {
+            console.log('Got back live url:', url);
+            onSuccess(url)
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+}
+
+function onChangeFont(font) {
+    setFont(font)
+    renderMeme()
+}
+
+function onChangeAlign(value) {
+    setAlign(value)
+    // renderMeme()
+}
+
+// function onKeywordsSearch(string) {
+//     let searchOptions = getFilteredKeywords(string).reduce((acc, option) => {
+//         acc += `<option value="${option.word}">`;
+//         return acc;
+//     }, '');
+//     document.querySelector('#search-options').innerHTML = searchOptions;
+//     updateSearchFilter(string);
+//     initImagesGallery();
+// }
+
+function onDeleteLine() {
+    deleteLine()
+    renderMeme()
+}
+
+
+function drawRect(x, y, xEnd, yEnd) {
+    gCtx.beginPath()
+    gCtx.rect(x, y, xEnd, yEnd);
+    gCtx.closePath()
+    gCtx.lineWidth = 2;
+    gCtx.strokeStyle = 'black';
+    gCtx.stroke();
+    renderMeme()
+}
+
+function onSetFocus() {
+    checkAlignFocus()
+}
+
+function onMoveLine(value) {
+    moveLine(value)
+    renderMeme()
+}
--- js/meme.service.js
+++ js/meme.service.js
@@ -6,11 +6,26 @@ var gMeme = {
     selectedLineIdx: 0,
     lines: [
         {
-            txt: 'I sometimes eat Falafel',
+            txt: 'TEXT COMES HERE',
+            font: 'Impact',
             size: 30,
             align: 'center',
-            color: 'red'
+            color: 'white',
+            x: 200,
+            y: 57,
+            isDrag: false
+        },
+        {
+            txt: 'TEXT COMES HERE',
+            font: "Impact",
+            size: 30,
+            align: 'center',
+            color: 'white',
+            x: 200,
+            y: 370,
+            isDrag: false
         }
+
     ]
 }
 function getMeme() {
@@ -18,9 +33,102 @@ function getMeme() {
 }
 
 function setLineTxt(txt) {
-    gMeme.lines[0].txt = txt
+    // if (!lines[currLine].txt) return
+    console.log('selectedLineIdx', gMeme.selectedLineIdx)
+    let currLine = gMeme.selectedLineIdx
+    console.log(currLine)
+    gMeme.lines[currLine].txt = txt
 }
 
-function setMeme(imgId) {
+function setImg(imgId) {
     gMeme.selectedImgId = imgId
 }
+
+function changeTxtColor(color) {
+    let currLine = gMeme.selectedLineIdx
+    gMeme.lines[currLine].color = color
+}
+
+function setFontSize(value) {
+    let currLine = gMeme.selectedLineIdx
+    gMeme.lines[currLine].size += value
+}
+
+function addNewTextLine() {
+    gMeme.lines.push(_createLine())
+}
+
+function _createLine() {
+    gMeme.selectedLineIdx = gMeme.lines.length
+
+    return {
+        txt: 'Add Text',
+        font: 'Impact',
+        size: 30,
+        align: 'center',
+        color: 'white',
+        x: 200,
+        y: 220,
+        isDrag: false
+    }
+}
+
+function switchLine() {
+    console.log(gMeme.selectedLineIdx)
+    let newLine = gMeme.selectedLineIdx++
+    if (gMeme.selectedLineIdx >= gMeme.lines.length) newLine = gMeme.selectedLineIdx = 0
+    console.log(newLine)
+    // return newLine
+}
+
+function setFont(font) {
+    let currLine = gMeme.selectedLineIdx
+    gMeme.lines[currLine].font = font
+}
+
+function setAlign(align) {
+    let currLine = gMeme.selectedLineIdx
+    log(align)
+    gMeme.lines[currLine].align = align
+
+}
+
+function deleteLine() {
+    console.log(gMeme.lines)
+    let currLine = gMeme.selectedLineIdx
+    gMeme.lines.splice(currLine, 1)
+    console.log(gMeme.lines)
+}
+
+function checkAlignFocus() {
+    let currLine = gMeme.selectedLineIdx
+    console.log(currLine)
+    let txt = gMeme.lines[currLine].txt
+    console.log(txt)
+    let x = gElCanvas.width / 2
+    let y = gMeme.lines[currLine].y
+    console.log('x', x, 'y', y)
+    console.log(gMeme.lines[currLine].align)
+    switch (gMeme.lines[currLine].align) {
+        case 'center':
+            drawRect(x - gCtx.measureText(txt).width / 2, y - parseInt(gCtx.font) * 1.5, gCtx.measureText(txt).width, parseInt(gCtx.font) * 2)
+            break;
+        case 'left':
+            drawRect(x, y - parseInt(gCtx.font) * 1.5, gCtx.measureText(txt).width, parseInt(gCtx.font) * 2)
+            break;
+        case 'right':
+            drawRect(x - gCtx.measureText(txt).width, y - parseInt(gCtx.font) * 1.5, gCtx.measureText(txt).width, parseInt(gCtx.font) * 2)
+            break;
+    }
+}
+
+
+function moveLine(value) {
+    let currLine = gMeme.selectedLineIdx
+    console.log(value)
+    console.log('gMeme[currLine].line', gMeme.lines[currLine].y)
+    if (gMeme.lines[currLine].y < 60 && value < 0) return
+    else if (gMeme.lines[currLine].y > 370 && value > 0) return
+    gMeme.lines[currLine].y += value
+}
+
